f50898ed4decc38c107384ce427add474605c3d8
Abrar-0 <abrarzawad222@gmail.com>
2024-03-12 12:13:23 +0600

    kk

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,37 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Todo from './todo'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Device name = "laptop" price="34k"></Device>
+      <Device name = "phone" price="23k"></Device>
       <Student></Student>
+      <Todo task="Learn React"></Todo>
+      <Todo task="Core Concepts"></Todo>
+      <Todo task="Die"></Todo>
     </>
   )
+}
+
+function Device(props){
+  const style1={
+    listStyleType:'none'
+  }
+  return <h2>This device: {props.name}, price : {props.price}</h2>
 }
 
 function Student(){
@@ -39,7 +41,9 @@ function Student(){
     age: 29,
     money: false
   }
-  const old = 23
-  return <h3>Hello I am {student.name}, my age is {old} and im {student.money? 'rich' : 'poor' }</h3>
+  const old = 26
+  return <div className='student'>
+  <h3>Hello I am {student.name}, my age is {old} and im {student.money? 'rich' : 'poor' }</h3>
+  </div>
 }
 export default App
---
+++ src/todo.jsx
@@ -0,0 +1,3 @@
+export default function Todo({task}){
+    return <li style={style1}>Task: {task}</li>
+}
